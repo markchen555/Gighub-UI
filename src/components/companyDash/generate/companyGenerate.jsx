a25c700b4f879ca50cd0c850aabb2def1cf36806
Daniel Chong <danchong625@gmail.com>
2018-03-07 20:36:10 -0800

    [feat] initializes component to generate key for recruiter

---
+++ src/components/companyDash/generate/companyGenerate.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class CompanyGenerate extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Generate key
+      </div>
+    )
+  }
+};
+
+export default CompanyGenerate;
