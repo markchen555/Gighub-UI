85688679420b28e7447cb2739dee7216d3ef6da2
Daniel Chong <danchong625@gmail.com>
2018-03-07 18:14:16 -0800

    [style] adds bootstrap styling

--- src/components/auth/companySignup.jsx
+++ src/components/auth/companySignup.jsx
@@ -4,17 +4,17 @@ const CompanySignup = ({ type, setUsername, setPW, setPWCheck, checkPW, push, si
   <form>
     <div className="form-group">
       <label htmlFor="auth-sign_username">Company Name</label>
-      <input type="text" id="auth-sign_username" onChange={(e) => setUsername(e.target.value)} />
+      <input type="text" id="auth-sign_username" className="form-control" onChange={(e) => setUsername(e.target.value)} />
     </div>
     <div className="form-group">
       <label htmlFor="auth-sign_pw">Password</label>
-      <input type="password" id="auth-sign_pw" onChange={(e) => setPW(e.target.value)} />
+      <input type="password" id="auth-sign_pw" className="form-control" onChange={(e) => setPW(e.target.value)} />
     </div>
     <div className="form-group">
       <label htmlFor="auth-sign_pwCheck">Re-type Password</label>
-      <input type="password" id="auth-sign_pwCheck" onChange={(e) => setPWCheck(e.target.value)} />
+      <input type="password" id="auth-sign_pwCheck" className="form-control" onChange={(e) => setPWCheck(e.target.value)} />
     </div>
-    <div className="form-group">
+    <div className="form-group login-submit">
       <button type="button" className="btn btn-outline-info" onClick={(e) => {
         e.preventDefault();
         push('/login');
