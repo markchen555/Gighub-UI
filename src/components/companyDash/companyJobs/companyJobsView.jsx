ba0b2afed22c9ebf16b64e4217cbe8c5c9d2ec64
Daniel Chong <danchong625@gmail.com>
2018-03-07 20:35:54 -0800

    [feat] initializes jobs view for company

---
+++ src/components/companyDash/companyJobs/companyJobsView.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class CompanyJobsView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Jobs View
+      </div>
+    )
+  }
+};
+
+export default CompanyJobsView;
