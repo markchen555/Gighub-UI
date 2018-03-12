2de0c871ff4f6432e8f08cdc656b65f6bd09d5bc
Mark Chen <markchen555@gmail.com>
2018-03-11 20:03:05 -0700

    Doc: Implemented handelChange to save the states in to nested state

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import axios from 'axios';
+import Loading from '../loading/loading.jsx'
 
 import './profile.css';
 
@@ -11,29 +12,39 @@ class ProfilePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      bio: null,
+      data: null,
     }
 
     this.fetch = this.fetch.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-   //this.fetch()
+   this.fetch()
   }
 
   fetch() {
-    axios.get(`${API_SERVER}/api/user/profile/${this.props.userId}}`)
+    axios.get(`${API_SERVER}/api/user/profile/${this.props.userId}`)
     .then(items => {
-      this.setState({ bio: items.data });
+      this.setState({ data: items.data });
     })
     .catch(err => {
       console.log('Fetch err:', err);
     })
   }
 
-  edit() {
-    axios.put(`${API_SERVER}/edit`)
-    .then(items => {
+  handleSubmit() {
+    axios({
+      method: 'POST',
+      url: `${API_SERVER}/api/user/edit`,
+      headers: {
+        "Authorization": `${this.props.token}`,
+      },
+      data: this.state.data.Bio
+    })
+    .then(() => {
+      this.fetch();
       console.log('Edit Success!');
     })
     .catch(err => {
@@ -41,8 +52,22 @@ class ProfilePage extends Component {
     })
   }
 
+  handleChange(e) {
+    e.preventDefault();
+    let inputName = e.target.name;
+    let inputValue = e.target.value;
+    let dataCopy = Object.assign({}, this.state);
+    dataCopy.data.Bio[inputName] = inputValue;
+    console.log(`this is bio copy`, JSON.stringify(dataCopy, null, 2))
+    this.setState(dataCopy);
+  }
+
   render() {
+    console.log(this.state)
     console.log(this.props.userId, 'this is props')
+    if(!this.state.data) {
+      return (<Loading />)
+    } 
     return (
           <div className="container profile-wrapper">
             <div className="row my-2">
@@ -62,85 +87,79 @@ class ProfilePage extends Component {
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">First name</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext" value={this.state.data.firstName}></input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Last name</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext" value={this.state.data.lastName}></input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Email</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext" value={this.state.data.email}></input>
                                 </div>
                               </div>
-                              <div className="form-group row">
+                             <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Phone Number</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext" >{this.state.data.phoneNumber}</input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Location</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext">{this.state.data.location}</input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Headline</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext" >{this.state.data.headline}</input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Experience Level</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext">{this.state.data.experience}</input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Education Level</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext"></input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Linkedin</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext" >{this.state.data.linkedIn}</input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Twitter</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
-                                </div>
-                              </div>
-                              <div className="form-group row">
-                                <label className="col-lg-3 col-form-label form-control-label">Facebook</label>
-                                <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext">{this.state.data.twitter}</input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Github</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext">{this.state.data.github}</input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Personal Portfolio</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext">{this.state.data.personal}</input>
                                 </div>
                               </div>
                               <div className="form-group row">
                                 <label className="col-lg-3 col-form-label form-control-label">Desire Salary</label>
                                 <div className="col-lg-9">
-                                  <input type="text" readOnly className="form-control-plaintext" id="staticEmail" value={this.props.userId}></input>
+                                  <input type="text" readOnly className="form-control-plaintext" ></input>
                                 </div>
                               </div>
                             </form>
@@ -148,45 +167,27 @@ class ProfilePage extends Component {
                         <div className="tab-pane" id="edit">
                             <form role="form">
                                 <div className="form-group row">
-                                    <label className="col-lg-3 col-form-label form-control-label">First name</label>
-                                    <div className="col-lg-9">
-                                        <input className="form-control" type="text" value=""></input>
-                                    </div>
-                                </div>
-                                <div className="form-group row">
-                                    <label className="col-lg-3 col-form-label form-control-label">Last name</label>
-                                    <div className="col-lg-9">
-                                        <input className="form-control" type="text" value=""></input>
-                                    </div>
-                                </div>
-                                <div className="form-group row">
-                                    <label className="col-lg-3 col-form-label form-control-label">Email</label>
-                                    <div className="col-lg-9">
-                                        <input className="form-control" type="email" value=""></input>
-                                    </div>
-                                </div>
-                                <div className="form-group row">
                                     <label className="col-lg-3 col-form-label form-control-label">Phone Number</label>
                                     <div className="col-lg-9">
-                                        <input className="form-control" type="text" value=""></input>
+                                        <input className="form-control" type="text" name="phoneNumber" onChange ={this.handleChange}></input>
                                     </div>
                                 </div>
                                 <div className="form-group row">
                                     <label className="col-lg-3 col-form-label form-control-label">Location</label>
                                     <div className="col-lg-9">
-                                        <input className="form-control" type="text" value="" ></input>
+                                        <input className="form-control" type="text" name="location" onChange ={this.handleChange} ></input>
                                     </div>
                                 </div>
                                 <div className="form-group row">
                                     <label className="col-lg-3 col-form-label form-control-label">Headline</label>
                                     <div className="col-lg-9">
-                                        <input className="form-control" type="text"></input>
+                                        <input className="form-control" type="text" name="headline" onChange ={this.handleChange}></input>
                                     </div>
                                 </div>
                                 <div className="form-group row">
                                     <label className="col-lg-3 col-form-label form-control-label">Experience Level</label>
                                     <div className="col-lg-9">
-                                        <select id="user_time_zone" className="form-control" size="0">
+                                        <select className="form-control" size="0">
                                             <option value="Entry">Entry</option>
                                             <option value="Intermediate">Intermediate</option>
                                             <option value="Advance">Advance</option>
@@ -197,7 +198,7 @@ class ProfilePage extends Component {
                               <div className="form-group row">
                                   <label className="col-lg-3 col-form-label form-control-label">Education Level</label>
                                   <div className="col-lg-9">
-                                      <select id="user_time_zone" className="form-control" size="0">
+                                      <select className="form-control" size="0">
                                           <option value="High School">High school diploma or equivalent</option>
                                           <option value="College With No Degree">Some college, no degree</option>
                                           <option value="Postsecondary noo=n-degree award">Postsecondary non-degree award</option>
@@ -211,37 +212,31 @@ class ProfilePage extends Component {
                               <div className="form-group row">
                                   <label className="col-lg-3 col-form-label form-control-label">Linkedin</label>
                                   <div className="col-lg-9">
-                                      <input className="form-control" type="text" value=""></input>
+                                      <input className="form-control" type="text" name="linkedIn" onChange ={this.handleChange}></input>
                                   </div>
                               </div>
                               <div className="form-group row">
                                   <label className="col-lg-3 col-form-label form-control-label">Twitter</label>
                                   <div className="col-lg-9">
-                                      <input className="form-control" type="text" value=""></input>
-                                  </div>
-                              </div>
-                              <div className="form-group row">
-                                  <label className="col-lg-3 col-form-label form-control-label">Facebook</label>
-                                  <div className="col-lg-9">
-                                      <input className="form-control" type="text" value=""></input>
+                                      <input className="form-control" type="text" name="twitter" onChange ={this.handleChange}></input>
                                   </div>
                               </div>
                               <div className="form-group row">
                                   <label className="col-lg-3 col-form-label form-control-label">Github</label>
                                   <div className="col-lg-9">
-                                      <input className="form-control" type="text" value=""></input>
+                                      <input className="form-control" type="text" name="github" onChange ={this.handleChange}></input>
                                   </div>
                               </div>
                               <div className="form-group row">
                                   <label className="col-lg-3 col-form-label form-control-label">Personal Portfolio</label>
                                   <div className="col-lg-9">
-                                      <input className="form-control" type="text" value=""></input>
+                                      <input className="form-control" type="text" name="personal" onChange ={this.handleChange}></input>
                                   </div>
                               </div>
                               <div className="form-group row">
                                   <label className="col-lg-3 col-form-label form-control-label">Desire Salary</label>
                                   <div className="col-lg-9">
-                                      <select id="user_time_zone" className="form-control" size="0">
+                                      <select className="form-control" size="0">
                                           <option value="High School">High school diploma or equivalent</option>
                                           <option value="College With No Degree">Some college, no degree</option>
                                           <option value="Postsecondary noo=n-degree award">Postsecondary non-degree award</option>
@@ -257,7 +252,7 @@ class ProfilePage extends Component {
                                   <label className="col-lg-3 col-form-label form-control-label"></label>
                                   <div className="col-lg-9">
                                       <input type="reset" className="btn btn-secondary" value="Cancel"></input>
-                                      <input type="button" className="btn btn-primary" value="Save Changes"></input>
+                                      <input type="button" className="btn btn-primary" value="Save Changes" onClick={this.handleSubmit}></input>
                                   </div>
                               </div>
                           </form>
@@ -269,7 +264,7 @@ class ProfilePage extends Component {
                   <h6 className="mt-2">Upload a different photo</h6>
                   <label className="custom-file">
                       <input type="file" id="file" className="custom-file-input"></input>
-                      <button className="custom-file-control">Choose file</button>
+                      <button className="custom-file-control btn btn-secondary">Choose file</button>
                   </label>
               </div>
           </div>
